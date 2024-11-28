52099d556fd050a09e9c32327599f2b055c74e01
Bekiboo <julien.connault@gmail.com>
2024-11-28 15:25:06 +0300

    Add lodash dependency and implement user tracking with socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,89 @@
 import io from 'socket.io-client'
 import './App.css'
-import { useEffect } from 'react'
-import Input from './components/input'
+import _ from 'lodash'
+import { useEffect, useRef, useState } from 'react'
+
+const generateRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
 
 function App() {
-  const socket = io('http://localhost:3000')
-
-  function connectSocket() {
-    socket.on('connection', (socket) => {
-      console.log('Hello!')
-    })
-  }
-
-  function handleInput(event) {
-    const { name, value } = event.target
-    console.log({ [name]: value })
-  }
+  const socket = useRef(null)
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    connectSocket()
+    socket.current = io('http://localhost:3000')
+
+    socket.current.on('connect', () => {
+      console.log('Connected to the server!')
+
+      socket.current.on('userId', (data) => {
+        setUser(data.id)
+      })
+
+      socket.current.on('mouseMove', (data) => {
+        const { id, x, y } = data
+        setUsers((prev) =>
+          prev.map((user) => (user.id === id ? { ...user, x, y } : user))
+        )
+      })
+
+      // handle current users
+      socket.current.on('usersConnected', (data) => {
+        console.log('Current Users:', data)
+        const users = data.map((id) => ({
+          id,
+          x: -100,
+          y: -100,
+          color: generateRandomColor(),
+        }))
+        setUsers(users)
+      })
+      socket.current.on('newUserConnected', (data) => {
+        setUsers((prev) => [...prev, data.id])
+      })
+      socket.current.on('userDisconnected', (data) => {
+        console.log('User Disconnected:', data.id)
+        setUsers((prev) => prev.filter((user) => user.id !== data.id))
+      })
+    })
+
+    return () => {
+      socket.current.disconnect()
+    }
   }, [])
+
+  function handleMouseMove(event) {
+    const { clientX, clientY } = event
+    socket.current.emit('mouseMove', { x: clientX, y: clientY })
+  }
+
   return (
-    <>
-      <h1>React Multiplayer Dashboard</h1>
-      <Input
-        name="name"
-        placeholder="Enter your name"
-        handleInput={handleInput}
-      />
-      <Input
-        name="score"
-        placeholder="Enter your score"
-        handleInput={handleInput}
-      />
-    </>
+    <div className="App" onMouseMove={_.throttle(handleMouseMove, 50)}>
+      <h2>
+        {users.length} user{users.length > 1 && 's'} connected
+      </h2>
+      {users.map(
+        (userr) =>
+          userr.id !== user && (
+            <div
+              key={userr.id}
+              className="user"
+              style={{
+                top: userr.y,
+                left: userr.x,
+                backgroundColor: userr.color,
+              }}
+            />
+          )
+      )}{' '}
+    </div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
-const {createServer} = require('http');
-const { Server } = require('socket.io');
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 
-const httpServer = createServer();
-const socket = new Server(httpServer, {
+const httpServer = createServer()
+const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173'
-  }
-});
+    origin: 'http://localhost:5173',
+  },
+})
 
-socket.on('connection', (socket) => {
-  console.log('Connected to server!');
-});
+let currentUsers = []
+
+io.on('connection', (socket) => {
+  console.log('User Connected:', socket.id)
+  currentUsers.push(socket.id)
+
+  // handle current users
+  socket.emit('userId', { id: socket.id })
+  socket.emit('usersConnected', currentUsers)
+
+  socket.broadcast.emit('usersConnected', currentUsers)
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected:', socket.id)
+    currentUsers = currentUsers.filter((user) => user !== socket.id)
+
+    socket.broadcast.emit('userDisconnected', { id: socket.id })
+  })
+
+  socket.on('mouseMove', (data) => {
+    socket.broadcast.emit('mouseMove', { ...data, id: socket.id })
+  })
+})
 
 httpServer.listen(3000, () => {
-  console.log('Server Connected');
-});
+  // console.log('Server Connected')
+})
